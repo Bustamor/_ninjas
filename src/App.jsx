7a21e1b1e4975ceecdc50e5f6f5f97c926c3b1b1
Bustamor <randall.bustamante@gmail.com>
2022-10-25 12:30:14 -0700

    init_first_react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>Things I need to do:</h2>
+
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marthon</li>
+        <li>Feed the dogs</li>
+      
+    </div>
+  );
+}
+
+export default App;
